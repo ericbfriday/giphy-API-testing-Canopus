b5aad916f45f23ac39910c6ab45aa6548576adf2
Eric Friday <ericfriday@gmail.com>
2017-10-03 13:42:22 -0500

    successfully retrieving gif object from giphy API using obscured api key

--- modules/routes/giphy.js
+++ modules/routes/giphy.js
@@ -3,11 +3,34 @@ var router = require('express').Router();
 // var path = require('path');
 // var mongoose = require('mongoose');
 var bodyParser = require( 'body-parser');
+var request = require('request');
+
+require('dotenv').config();
 
 router.use(bodyParser.json());
 router.use(bodyParser.urlencoded({
     extended: true
 }));
+
+// var url = 'https://api.giphy.com/v1/gifs/search?api_key=HW5jgIncwNEWpawEIuBgYQIeB3bupvbo&q=&limit=25&offset=0&rating=G&lang=en';
+// var options = {
+//     url: url,
+//     method: 'GET',
+// };
+
+router.get('/', function(req, res){
+    console.log('Inside giphy.js router GET function!');
+    console.log('logging GIPHY_KEY', process.env.GIPHY_KEY);
+    request('https://api.giphy.com/v1/gifs/search?api_key=' + process.env.GIPHY_KEY + '&q=cute+kitten&limit=25&offset=0&rating=G&lang=en', function(error, response, body) {
+        console.log('logging error', error);
+        // console.log('logging response', response);
+        console.log('logging body ', body);
+
+        res.status(200).send(JSON.parse(body));
+    });
+
+    // res.sendStatus(200);
+});
 
 
 module.exports = router;
